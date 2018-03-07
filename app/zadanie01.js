04081c197151d03ef5330bd52e6b389cb5381018
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-07 11:25:46 +0100

    Ex. 01 done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,16 @@
-//Twój kod
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
+app.use(express.static('./public/zadanie01/'));
+
+app.post('/', (req, res) => {
+    const {numberA, numberB} = req.body;
+    (numberA % numberB === 0 )? (res.send(`${numberB} jest dzielnikiem ${numberA}`))
+        : (res.send(`${numberB} nie jest dzielnikiem ${numberA}`));
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
